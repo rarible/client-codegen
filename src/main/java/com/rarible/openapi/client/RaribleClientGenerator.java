acd634a74864c8c87ece1aff92f5f7d9d3b6ee77
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-05-05 23:29:48 +0300

    initial client codegen

---
+++ src/main/java/com/rarible/openapi/client/RaribleClientGenerator.java
@@ -0,0 +1,41 @@
+package com.rarible.openapi.client;
+
+import org.openapitools.codegen.CodegenConfig;
+import org.openapitools.codegen.languages.JavaClientCodegen;
+import org.openapitools.codegen.languages.KotlinSpringServerCodegen;
+
+import java.util.stream.Collectors;
+
+public class RaribleClientGenerator extends JavaClientCodegen implements CodegenConfig {
+
+  /**
+   * Configures a friendly name for the generator.  This will be used by the generator
+   * to select the library with the -g flag.
+   * 
+   * @return the friendly name for the generator
+   */
+  public String getName() {
+    return "rarible-client";
+  }
+
+  public RaribleClientGenerator() {
+    super();
+    embeddedTemplateDir = templateDir = "rarible-client";
+    setModelPackage("com.rarible.protocol.dto");
+    setLibrary("webclient");
+    setModelNameSuffix("Dto");
+    setUseBeanValidation(false);
+    setOpenApiNullable(false);
+  }
+
+  @Override
+  public void processOpts() {
+    super.processOpts();
+    supportingFiles = supportingFiles.stream().filter(it -> !it.getTemplateFile().toLowerCase().contains("gradle")).collect(Collectors.toList());
+    importMapping.put("Address", "scalether.domain.Address");
+    importMapping.put("Word", "io.daonomic.rpc.domain.Word");
+    importMapping.put("Binary", "io.daonomic.rpc.domain.Binary");
+    importMapping.put("BigInteger", "java.math.BigInteger");
+    importMapping.put("BigDecimal", "java.math.BigDecimal");
+  }
+}
