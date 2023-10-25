181c0e38dedca42d1dba3916304ee0718d4ee9a9
Artem Yakischik <a.yakischik@rarible.com>
2023-10-25 17:41:57 +0400

    feat: x-controller support

--- src/main/java/com/rarible/openapi/client/RaribleTsClientGenerator.java
+++ src/main/java/com/rarible/openapi/client/RaribleTsClientGenerator.java
@@ -3,14 +3,12 @@ package com.rarible.openapi.client;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
-import org.openapitools.codegen.languages.JavaClientCodegen;
-import org.openapitools.codegen.templating.mustache.CamelCaseLambda;
+import org.openapitools.codegen.languages.TypeScriptFetchClientCodegen;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-public class RaribleClientGenerator extends JavaClientCodegen {
+public class RaribleTsClientGenerator extends TypeScriptFetchClientCodegen {
 
     private static final String X_CONTROLLER = "x-controller";
 
@@ -22,7 +20,7 @@ public class RaribleClientGenerator extends JavaClientCodegen {
      */
     @Override
     public String getName() {
-        return "rarible-client";
+        return "rarible-typescript-fetch";
     }
 
     private final Map<String, String> tagMapping = new HashMap<>();
@@ -56,29 +54,5 @@ public class RaribleClientGenerator extends JavaClientCodegen {
             return controller;
         }
         return super.sanitizeTag(tag);
-    }
-
-    public RaribleClientGenerator() {
-        embeddedTemplateDir = templateDir = "rarible-client";
-        setModelPackage("com.rarible.protocol.dto");
-        setLibrary("webclient");
-        setModelNameSuffix("Dto");
-        setUseBeanValidation(false);
-        setOpenApiNullable(false);
-        additionalProperties().put("firstLetterUppercase", new CamelCaseLambda(false));
-    }
-
-    @Override
-    public void processOpts() {
-        super.processOpts();
-        supportingFiles = supportingFiles.stream().filter(it -> !it.getTemplateFile().toLowerCase().contains("gradle")).collect(Collectors.toList());
-        typeMapping.put("OffsetDateTime", "Instant");
-        typeMapping.put("java.time.OffsetDateTime", "java.time.Instant");
-        importMapping.put("OffsetDateTime", "java.time.Instant");
-        importMapping.put("Address", "scalether.domain.Address");
-        importMapping.put("Word", "io.daonomic.rpc.domain.Word");
-        importMapping.put("Binary", "io.daonomic.rpc.domain.Binary");
-        importMapping.put("BigInteger", "java.math.BigInteger");
-        importMapping.put("BigDecimal", "java.math.BigDecimal");
     }
 }
